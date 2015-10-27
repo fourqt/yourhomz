3e677484656b4a359f2750cb704c79f01844ba14
Prafull Sharma <prafull.sharma@4qt.com>
2015-10-27 18:05:20 +0530

    sidebar code finished

--- rooffers/themes/default/footer.php
+++ rooffers/themes/default/footer.php
@@ -9,14 +9,16 @@
     <div id="side-backdrop"></div>
     <div id="side-nav-menu">
         <ul class="list-group">
-            <li class="text-uppercase"><a href="javascript:void(0);">Home</a></li>
-            <li class="text-uppercase"><a title="Know more about Roofers.com" href="javascript:void(0);" data-toggle="tooltip" data-placement="bottom">About Us</a></li>
-            <li class="text-uppercase"><a href="javascript:void(0);">Blog</a></li>
+            <li class="text-uppercase"><a href="javascript:void(0);"><strong>Home</strong></a></li>
+            <li class="text-uppercase"><a href="javascript:void(0);"><strong>My Profile</strong></a></li>
+            <li class="text-uppercase"><a title="Know more about Roofers.com" href="javascript:void(0);" ><strong>About Us</strong></a></li>
+            <li class="text-uppercase"><a href="javascript:void(0);"><strong>News</strong></a></li>
+            <li class="text-uppercase"><a href="javascript:void(0);"><strong>ShortListed</strong></a></li>
         </ul>
         <ul class="list-group">
-            <li class="full-link menu-list-item"><a title="Get assets of logo, images of Roofers.com" rel="nofollow" href="/media_kit.zip" data-toggle="tooltip" data-placement="bottom">Media Kit</a></li>
+            <li><a title="Get assets of logo, images of Roofers.com" rel="nofollow" href="javascript:void(0);">Media Kit</a></li>
             
-            <li class="full-link last-link menu-list-item padded-item">
+            <li>
                 <a data-bypass="true" href="mailto:support@roofers.com" class="support-email">
                     <div class="call-text">Need any help? Write to us at</div>
                     <div class="circled"><i class="icon icon-message"></i><span>support@roofers.com </span></div>
@@ -24,14 +26,24 @@
             </li>
         </ul>
         <ul class="list-group">
-            <li class="bordered-item last-link menu-list-item padded-item">
-                <div class="text">Follow Us</div>
+            <li>
+                <div class="text"><h3>Follow Us</h3></div>
                 <div class="share-icons">
-                    <a rel="nofollow" target="_blank" href="https://twitter.com/roofers" data-bypass="true" class="link"><i class="icon icon-twitter"></i></a>
-                    <a rel="nofollow" target="_blank" href="http://www.facebook.com/roofers.co.in" data-bypass="true" class="link"><i class="icon icon-facebook"></i></a>
-                    <a target="_blank" rel="publisher" href="https://plus.google.com/116390109264258994237" data-bypass="true" class="link"><i class="icon icon-gplus"></i></a>
-                    <a target="_blank" rel="publisher" href="https://instagram.com/roofersindia/" data-bypass="true" class="link"><i class="icon icon-instagram"></i></a>
-                    <a target="_blank" rel="publisher" href="https://www.pinterest.com/roofersindia/" data-bypass="true" class="link"><i class="icon icon-pinterest"></i></a>
+                    <a rel="nofollow" target="_blank" href="javascript:void(0);" class="social tw">
+                        <i class="fa fa-twitter-square fa-2x fa-fw"></i>
+                    </a>
+                    <a rel="nofollow" target="_blank" href="javascript:void(0);" class="social fb">
+                        <i class="fa fa-facebook-square fa-2x fa-fw"></i>
+                    </a>
+                    <a rel="publisher" target="_blank" href="javascript:void(0);" class="social gp">
+                        <i class="fa fa-google-plus-square fa-2x fa-fw"></i>
+                    </a>
+                    <a rel="publisher" target="_blank" href="javascript:void(0);" class="social ig">
+                        <i class="fa fa-instagram fa-2x fa-fw"></i>
+                    </a>
+                    <a rel="publisher" target="_blank" href="javascript:void(0);" class="social pint">
+                        <i class="fa fa-pinterest-square fa-2x fa-fw"></i>
+                    </a>
                 </div>
             </li>
         </ul>
