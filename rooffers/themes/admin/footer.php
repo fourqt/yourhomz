71b84e4b446ce4e7bc2c50a90b784bfe8ff48bb8
Prafull Sharma <prafull.sharma@4qt.com>
2015-10-31 14:16:21 +0530

    fixing admin header footer part

--- rooffers/themes/admin/footer.php
+++ rooffers/themes/admin/footer.php
@@ -1,13 +1,69 @@
-	<footer class="container-fluid footer">
-		<p class="pull-right">
-			Executed in {elapsed_time} seconds, using {memory_usage}.<br />
-			Powered by <a href="http://cibonfire.com" target="_blank"><span class="icon-fire"></span>&nbsp;Rooffers</a> <?php echo BONFIRE_VERSION; ?>
-		</p>
-	</footer>
+	<div class="page-footer">
+		<div class="container">
+      	<p class="no-s">2015 &copy; Rooffers by 4qt. | Executed in {elapsed_time} seconds, using {memory_usage}.</p>
+      </div>
+   </div>
+</div><!-- Page Inner -->
+	</main><!-- Page Content -->
+<nav class="cd-nav-container" id="cd-nav">
+   <header>
+       <h3>Navigation</h3>
+       <a href="#0" class="cd-close-nav">Close</a>
+   </header>
+   <ul class="cd-nav list-unstyled">
+		<li class="cd-selected" data-menu="index">
+			<a href="javsacript:void(0);">
+		   	<span><i class="glyphicon glyphicon-home"></i></span>
+		      <p>Dashboard</p>
+			</a>
+		</li>
+		<li data-menu="profile">
+		  <a href="javsacript:void(0);">
+		      <span>
+		          <i class="glyphicon glyphicon-user"></i>
+		      </span>
+		      <p>Profile</p>
+		  </a>
+		</li>
+		<li data-menu="inbox">
+		  <a href="javsacript:void(0);">
+		      <span>
+		          <i class="glyphicon glyphicon-envelope"></i>
+		      </span>
+		      <p>Mailbox</p>
+		  </a>
+		</li>
+		<li data-menu="#">
+		  <a href="javsacript:void(0);">
+		      <span>
+		          <i class="glyphicon glyphicon-tasks"></i>
+		      </span>
+		      <p>Tasks</p>
+		  </a>
+		</li>
+		<li data-menu="#">
+		  <a href="javsacript:void(0);">
+		      <span>
+		          <i class="glyphicon glyphicon-cog"></i>
+		      </span>
+		      <p>Settings</p>
+		  </a>
+		</li>
+		<li data-menu="calendar">
+		  <a href="javsacript:void(0);">
+		      <span>
+		          <i class="glyphicon glyphicon-calendar"></i>
+		      </span>
+		      <p>Calendar</p>
+		  </a>
+		</li>
+   </ul>
+        </nav>
+        <div class="cd-overlay"></div>
 	<div id="debug"><!-- Stores the Profiler Results --></div>
-    <!-- Grab Google CDN's jQuery, with a protocol relative URL; fall back to local if offline 
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>-->
-    <script>window.jQuery || document.write('<script src="<?php echo js_path(); ?>jquery-2.1.4.min.js"><\/script>');</script>
+   <!-- Grab Google CDN's jQuery, with a protocol relative URL; fall back to local if offline 
+   <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>-->
+   <script>window.jQuery || document.write('<script src="<?php echo js_path(); ?>jquery-2.1.4.min.js"><\/script>');</script>
 	<?php echo Assets::js(); ?>
 </body>
 </html>
